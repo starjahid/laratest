a360672b8967be2fe270001d5d3b941cb43c4f95
starjahid <starjahid@yahoo.com>
2022-08-04 15:24:39 +0600

    Pagination with redis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+
 
 class HomeController extends BaseController
 {
@@ -17,24 +19,38 @@ class HomeController extends BaseController
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index(Request $request){
-
+        //dd(  $users = User::paginate(20));
         if(isset($request->birth_year) || isset($request->birth_month)){
-                $users = DB::table('users');
-            if(isset($request->birth_year) && isset($request->birth_month)){
-                $users = $users->whereYear('birthday', '=', $request->birth_year)
-                    ->whereMonth('birthday', '=', $request->birth_month);
-            }elseif (isset($request->birth_month)){
-                $users = $users->whereMonth('birthday', '=', $request->birth_month);
-            }else{
-                $users = $users->whereYear('birthday', '=', $request->birth_year);
+            $page=1; //Default value
+            if($request->get('page')){
+                $page = $request->get('page');
             }
-            $users = $users->paginate(20);
+            // Check In Redis
+            $birthYear=  isset($request->birth_year)?$request->birth_year:0;
+            $birthMonth=  isset($request->birth_month)?$request->birth_month:0;
+            $redisSearchKey =$birthYear.'_'.$birthMonth;
+            $users = unserialize(Redis::get("{$redisSearchKey}:{$page}"));
+            if(!$users):
+                $users = DB::table('users');
+                $users = $users->whereYear('birthday', '=', $birthYear)
+                    ->whereMonth('birthday', '=', $birthMonth);
+                $users = $users->paginate(20);
+                $userData = serialize($users);
+                Redis::set("{$redisSearchKey}:{$page}",$userData);
+                Redis::expire("{$redisSearchKey}:{$page}", 60);
+                $users = unserialize(Redis::get("{$redisSearchKey}:{$page}"));
+                //dd($users);
+            else:
+                $users = unserialize(Redis::get("{$redisSearchKey}:{$page}"));
+            endif;
+            //dd($users);
+            //return view('user.index',compact('users'));
         }else{
             $users = User::paginate(20);
         }
-        //return view('user.index',compact('users'));
         return view('user.index', [
             'users' => $users->appends(request()->query())
+
         ]);
     }
 }
--- config/database.php
+++ config/database.php
@@ -91,6 +91,19 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'redis' => [
+
+            'cluster' => false,
+            'client' => env('REDIS_CLIENT', 'phpredis'),
+            'default' => [
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', ''),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => 0,
+            ],
+
+        ],
+
     ],
 
     /*
